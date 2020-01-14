cf1ea43d626abca493f8e3034de619f2465da0af
Divyendu Singh <singh@prisma.io>
2020-01-14 22:09:05 +0100

    module cleanup

--- src/fs_utils.rs
+++ src/fs_utils.rs
@@ -1,6 +1,8 @@
-use std::env;
-use std::fs;
-use std::io;
+use std::io::Write;
+use std::result::Result;
+use std::{env, fs, io};
+
+use curl::easy::Easy;
 
 fn does_folder_exist(folder: &str) -> io::Result<bool> {
     let mut path = env::current_dir()?;
@@ -14,4 +16,11 @@ pub fn ensure_folder(folder: &str) -> io::Result<()> {
         true => Ok(()),                            // Do nothing
         false => Ok(fs::create_dir_all(&folder)?), // Try to create it
     }
+}
+
+pub fn download(from: &str, mut to: fs::File) -> Result<(), curl::Error> {
+    let mut easy = Easy::new();
+    easy.url(from)?;
+    easy.write_function(move |data| Ok(to.write(data).unwrap()))?;
+    easy.perform()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod common_utils;
 mod fs_utils;
 mod process_image;
 mod runtime_env;
@@ -8,24 +9,12 @@ use actix_files as afs;
 use actix_web::{get, web, App, HttpServer, Result};
 use std::fs;
 
-use std::io::Write;
-
-use curl::easy::Easy;
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
-
-fn calculate_hash<T: Hash>(t: &T) -> u64 {
-    let mut s = DefaultHasher::new();
-    t.hash(&mut s);
-    s.finish()
-}
-
 #[get("/")]
 async fn index(query: web::Query<HashMap<String, String>>) -> Result<afs::NamedFile> {
     let link = query.get("link");
     match link {
         Some(link) => {
-            let hash = calculate_hash(&link);
+            let hash = common_utils::calculate_hash(&link);
 
             let folder = String::from("static/");
             fs_utils::ensure_folder(&folder)?;
@@ -33,31 +22,34 @@ async fn index(query: web::Query<HashMap<String, String>>) -> Result<afs::NamedF
             // TODO: Support other file types
             let filename = hash.to_string() + ".png";
             let file_path = folder + &filename;
-            let mut output = fs::File::create(&file_path)?;
+            let output = fs::File::create(&file_path)?;
+
             // TODO: Download to buffer, don't write to file
-            let mut easy = Easy::new();
-            easy.url(link).unwrap();
-            easy.write_function(move |data| Ok(output.write(data).unwrap()))
-                .unwrap();
-            easy.perform().unwrap();
-            // TODO: Read from an in-memory buffer, not the file system
-            let img = image::open(&file_path).unwrap();
-            let img = process_image::run(img);
-            // TODO: Serve the buffer without writing to the file system
-            img.save(&file_path).unwrap();
-            Ok(afs::NamedFile::open(&file_path)?)
+            if fs_utils::download(link, output).is_err() {
+                return_error("Error: Failed to download the file to the file system")
+            } else {
+                // TODO: Read from an in-memory buffer, not the file system
+                let img = image::open(&file_path).unwrap();
+                let img = process_image::run(img);
+                // TODO: Serve the buffer without writing to the file system
+                img.save(&file_path)?;
+                Ok(afs::NamedFile::open(&file_path)?)
+            }
+
         }
         None => {
-            println!(
-                "Error: The get parameter link is not provided, please provide an image as a link to process"
-            );
-            // TODO: This should be a proper error message, once I figure out union types as return type
-            // TODO: Ensure, error.png is present after the build
-            // https://doc.rust-lang.org/std/macro.include_bytes.html (Need to alter the return type)
-            // https://github.com/pyros2097/rust-embed
-            Ok(afs::NamedFile::open("error.png")?)
+            return_error("Error: The get parameter link is not provided, please provide an image as a link to process")
         }
     }
+}
+
+fn return_error(msg: &str) -> Result<afs::NamedFile> {
+    println!("{:?}", msg);
+    // TODO: This should be a proper error message, once I figure out union types as return type
+    // TODO: Ensure, error.png is present after the build
+    // https://doc.rust-lang.org/std/macro.include_bytes.html (Need to alter the return type)
+    // https://github.com/pyros2097/rust-embed
+    Ok(afs::NamedFile::open("error.png")?)
 }
 
 #[actix_rt::main]
