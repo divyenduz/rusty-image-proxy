6b63d8a7a19932c3a9994de0f78a18a9f7570a4a
Divyendu Singh <singh@prisma.io>
2020-01-14 20:41:39 +0100

    error_handling: 1 notch better than before

--- src/main.rs
+++ src/main.rs
@@ -21,35 +21,41 @@ fn calculate_hash<T: Hash>(t: &T) -> u64 {
 
 #[get("/")]
 async fn index(query: web::Query<HashMap<String, String>>) -> Result<fs::NamedFile> {
-    let link = query.get("link").unwrap();
-    let hash = calculate_hash(&link);
-    /*
-        TODO:
-        Move files to static (or entirely remove their need)
-        Satan wrote rust because this is valid
-          let filename = hash.to_string() + ".png";
-        and this isn't
-          let filename = "static/" + hash.to_string() + ".png";
-    */
-    let filename = hash.to_string() + ".png";
-
-    let mut output = File::create(&filename)?;
-
-    // TODO: Download to buffer, don't write to file
-    let mut easy = Easy::new();
-    easy.url(link).unwrap();
-    easy.write_function(move |data| Ok(output.write(data).unwrap()))
-        .unwrap();
-    easy.perform().unwrap();
-
-    // TODO: Read from an in-memory buffer, not the file system
-    let img = image::open(&filename).unwrap();
-    let img = process_image::run(img);
-
-    // TODO: Serve the buffer without writing to the file system
-    img.save(&filename).unwrap();
-
-    Ok(fs::NamedFile::open(&filename)?)
+    let link = query.get("link");
+    match link {
+        Some(link) => {
+            let hash = calculate_hash(&link);
+            /*
+                TODO:
+                Move files to static (or entirely remove their need)
+                Satan wrote rust because this is valid
+                  let filename = hash.to_string() + ".png";
+                and this isn't
+                  let filename = "static/" + hash.to_string() + ".png";
+            */
+            let filename = hash.to_string() + ".png";
+            let mut output = File::create(&filename)?;
+            // TODO: Download to buffer, don't write to file
+            let mut easy = Easy::new();
+            easy.url(link).unwrap();
+            easy.write_function(move |data| Ok(output.write(data).unwrap()))
+                .unwrap();
+            easy.perform().unwrap();
+            // TODO: Read from an in-memory buffer, not the file system
+            let img = image::open(&filename).unwrap();
+            let img = process_image::run(img);
+            // TODO: Serve the buffer without writing to the file system
+            img.save(&filename).unwrap();
+            Ok(fs::NamedFile::open(&filename)?)
+        }
+        None => {
+            println!(
+                "Error: The get parameter link is not provided, please provide an image as a link to process"
+            );
+            // TODO: This should be a proper error message, once I figure out union types as return type
+            Ok(fs::NamedFile::open("error.png")?)
+        }
+    }
 }
 
 #[actix_rt::main]
@@ -60,11 +66,9 @@ async fn main() -> std::io::Result<()> {
     host.push_str(":");
     host.push_str(&port);
 
-    let bind_to = host; // Better variable name
-
-    println!("Listening on {:?}", bind_to);
+    println!("Listening on {:?}", host);
     HttpServer::new(|| App::new().service(index))
-        .bind(bind_to)?
+        .bind(host)?
         .run()
         .await
 }
