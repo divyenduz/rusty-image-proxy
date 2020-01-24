58eb51f47f579657d24db9e909d42320343a2ef4
Divyendu Singh <singh@prisma.io>
2020-01-24 23:07:33 +0100

    feat(formats): add support for jpeg, gif
    - a new unknown format will most likely fail now
    - fixes https://github.com/divyenduz/rusty-image-proxy/issues/2
    - adds support for debug env for logging

--- src/runtime_env.rs
+++ src/runtime_env.rs
@@ -1,5 +1,20 @@
 use std::env;
 
+pub fn get_debug() -> bool {
+    match env::var("DEBUG") {
+        // TODO: find better way to convert env string to bool
+        // https://github.com/softprops/envy
+        Ok(val) => {
+            if val.as_str() == "false" {
+                false
+            } else {
+                true
+            }
+        }
+        Err(_) => false,
+    }
+}
+
 pub fn get_bind_to_link() -> String {
     let mut host = env::var("HOST").unwrap_or("0.0.0.0".to_string());
     let port = env::var("PORT").unwrap_or("8088".to_string());
